17626cc7a395f5844ad5c3a5f7e5e7d88a4ad114
Sowmya Vijayendra <sowmyamv15@gmail.com>
2023-05-18 14:00:18 -0400

    Implemented counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  // --- our code goes here ---
+  $("#tweet-text").on("input", function (event) {
+    let textArea = this;
+    let charCount = textArea.value.length;
+    let counter = $(textArea).siblings("div").find(".counter");
+    $(counter).text(140 - charCount);
+    if (charCount > 140) {
+      $(counter).css("color", "red");
+    } else {
+      $(counter).css("color", "black");
+    }
+  });
+});
