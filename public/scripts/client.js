d4ad831c879a9bfcadf5e9d6cd2cb71ed8f47450
Sowmya Vijayendra <sowmyamv15@gmail.com>
2023-05-23 18:03:53 -0400

    fetching tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,31 +5,9 @@
  */
 $(document).ready(function () {
 // Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+
 const createTweetElement = function(tweetData){
+  let createddate =format(tweetData['created_at']);
   let $tweet = $(`  <article>
   <header>    
     <span>      
@@ -41,7 +19,7 @@ const createTweetElement = function(tweetData){
   <p>${tweetData['content']['text']}</p>
   <hr/> 
   <footer >
-    <div>${tweetData['created_at']}</div>
+    <div>${createddate}</div>
     <div>
       <i class="fa-solid fa-flag"></i>
       <i class="fa-solid fa-retweet"></i>
@@ -75,19 +53,23 @@ const renderTweets = function(tweets) {
         type: "POST",
         url: url,
         data: form.serialize(),
-        success: function(data) {
-              
-          renderTweets(data);
+        success: function(data) { 
             // Ajax call completed successfully
             alert("Form Submited Successfully");
         },
-        error: function(data) {
-              
+        error: function(data) {              
             // Some error in ajax call
             alert("some Error");
         }
     });
     });   
   });
+ const loadtweets = function(){
+  $.ajax('/tweets', { method: 'GET' })
+  .then(function (moreTweets) {
+    console.log('Success: ', moreTweets);
+    renderTweets(moreTweets);
+  });
+ };
 
 });
