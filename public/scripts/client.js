8537cf4f009055850a3b1fea15c00793c54371b6
Sowmya Vijayendra <sowmyamv15@gmail.com>
2023-05-25 17:59:35 -0400

    validation error formatting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,30 +1,28 @@
 //const { text } = require("express");
 
-
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function () {
-
   const escape = function (str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
 
-const createTweetElement = function(tweetData){
-  let createddate =timeago.format(tweetData['created_at']);
-  let $tweet = $(`  <article>
+  const createTweetElement = function (tweetData) {
+    let createddate = timeago.format(tweetData["created_at"]);
+    let $tweet = $(`  <article>
   <header>    
     <span>      
     <i class="fa-solid fa-user"></i>
-    <span>${tweetData['user']['name'] }</span>   
+    <span>${tweetData["user"]["name"]}</span>   
     </span>  
-  <span id="username">${tweetData['user']['handle']}</span>     
+  <span id="username">${tweetData["user"]["handle"]}</span>     
   </header>
-  <p>${escape(tweetData['content']['text'])}</p>
+  <p>${escape(tweetData["content"]["text"])}</p>
   <hr/> 
   <footer >
     <div>${createddate}</div>
@@ -35,60 +33,62 @@ const createTweetElement = function(tweetData){
     </div>          
   </footer>
 </article>`);
-  return $tweet;
-};
-const renderTweets = function(tweets) {
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
-  if(Array.isArray(tweets)){
-    tweets.reverse();
-    $('.tweet-container').empty();
-    tweets.forEach(element => {
-     const $tweet = createTweetElement(element);        
-      $('.tweet-container').append($tweet);
-    }); 
-  } 
-};
+    return $tweet;
+  };
+  const renderTweets = function (tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    if (Array.isArray(tweets)) {
+      tweets.reverse();
+      $(".tweet-container").empty();
+      tweets.forEach((element) => {
+        const $tweet = createTweetElement(element);
+        $(".tweet-container").append($tweet);
+      });
+    }
+  };
 
-
-
-   let form = $('#tweet-form');
-    form.submit((event)=>{
-      event.preventDefault(); // prevent default behavior    
-    let textArea = $("#tweet-text");      
-    if(!textArea.val() || textArea.val().length === 0){
-      alert("Tweet data is empty");
+  let form = $("#tweet-form");
+  form.submit((event) => {
+    event.preventDefault(); // prevent default behavior
+    $(".error").empty();
+    $(".error").hide();
+    let textArea = $("#tweet-text");
+    if (!textArea.val() || textArea.val().length === 0) {
+      $(".error").append("Tweet data is empty");
+      $(".error").slideDown(500);
       return;
-    }else if(textArea.val().length > 140){
-      alert("Tweet is too long!");
-      return ;
-    }  
-      var url = form.attr('action');
-      $.ajax({
-        type: "POST",
-        url: url,
-        data: form.serialize(),
-        success: function(data) { 
-            // Ajax call completed successfully
-            alert("Form Submited Successfully");
-            loadtweets();
-            textArea.val('');
-        },
-        error: function(data) {              
-            // Some error in ajax call
-            alert("some Error");
-        }
-    });
-    });   
-  
- const loadtweets = function(){
-  $.ajax('/tweets', { method: 'GET' })
-  .then(function (moreTweets) {
-    console.log('Success: ', moreTweets);
-    renderTweets(moreTweets);
-  });
- };
+    } else if (textArea.val().length > 140) {
+      $(".error").append("Tweet is too long!");
+      $(".error").slideDown(500);
+      return;
+    }
+    var url = form.attr("action");
+    $.ajax({
+      type: "POST",
+      url: url,
+      data: form.serialize(),
+      success: function (data) {
+        // Ajax call completed successfully
 
- loadtweets();
+        loadtweets();
+        textArea.val("");
+      },
+      error: function (data) {
+        // Some error in ajax call
+        $(".error").append("Some error!");
+        $(".error").slideDown(500);
+      },
+    });
+  });
+
+  const loadtweets = function () {
+    $.ajax("/tweets", { method: "GET" }).then(function (moreTweets) {
+      console.log("Success: ", moreTweets);
+      renderTweets(moreTweets);
+    });
+  };
+  $(".error").hide();
+  loadtweets();
 });
